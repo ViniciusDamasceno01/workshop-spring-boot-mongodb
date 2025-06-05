8e75e0befae61404f079851af83c7144ded95b09
Vinicius Damasceno <vinicius.poletti@ucsal.edu.br>
2025-06-05 12:09:43 -0300

    feat: Conectando ao MongoDB com repository e service

--- src/main/java/com/example/workshopmongo/resources/UserResource.java
+++ src/main/java/com/example/workshopmongo/resources/UserResource.java
@@ -4,23 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User vinicius = new User("1", "Vinicius", "vinis02p@gmail.com");
-		User maria = new User("2", "Maria", "maria@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(vinicius, maria));
-		return ResponseEntity.ok(list);
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 }
